7406aeade5e1a8451b4323426d6aac68cf0c0fd7
Eldad Chidra <eldadchb@gmail.com>
2023-03-14 14:13:38 +0100

    add: cv and profile picture

---
+++ src/app/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HelmetMeta } from "./HelmetMeta";
+import { ThemeProvider } from "../components/theme/ThemeProvider";
+import { CssBaseline } from "@material-ui/core";
+
+import { Home } from "../pages/Home";
+
+export const App = () => {
+
+    return (
+        <ThemeProvider>
+            <CssBaseline />
+            <Router>
+                <HelmetMeta />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                </Switch>
+            </Router>
+        </ThemeProvider>
+    );
+};
