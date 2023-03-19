f496c19b2f0e83d8f0cfbb506100dadba2d1f666
Eldad Chidra <eldadchb@gmail.com>
2023-03-19 20:07:20 +0100

    add: works/ctrip

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -5,14 +5,14 @@ import { Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { TextDecrypt } from "../content/TextDecrypt";
 
-import './Works.css';
+import "./Works.css";
 
-import Lacalle from '../../assets/projects/lacalle.png';
+import CTrip from "../../assets/projects/ctrip.png";
 
 const useStyles = makeStyles((theme) => ({
   main: {
-    maxWidth: '100vw',
-    marginTop: '3em',
+    maxWidth: "100vw",
+    marginTop: "3em",
     marginBottom: "auto",
   },
 }));
@@ -20,14 +20,19 @@ const useStyles = makeStyles((theme) => ({
 export const Works = () => {
   const classes = useStyles();
   const [projects, setProjects] = useState([
-    { 
-      id: 5,
-      title: 'LaCalle Cafe', 
-      description: `A website project for the La Calle Cafe business
-      built using Wordpress and PHP with integrated SEO tools to help
-      the business ramp up its prospects and lead generation.`,
-      alter: 'Startup Project',
-      image: `${Lacalle}`,
+    {
+      id: 1,
+      title: "CTrip",
+      description: `CTrip is an app that helps users plan 
+                    a short trip to a chosen destination 
+                    based on their preferences. 
+                    The app allows users to select the 
+                    types of restaurants and attractions 
+                    they would like to visit, and then 
+                    generates a customized route for 
+                    them to follow.`,
+      alter: "Startup Project",
+      image: `${CTrip}`,
     },
   ]);
 
@@ -35,17 +40,15 @@ export const Works = () => {
     <section id="works">
       <Container component="main" className={classes.main} maxWidth="md">
         {projects.map((project) => (
-          <div className="project" key={ project.id }>
+          <div className="project" key={project.id}>
             <div className="__img_wrapper">
-              <img src={ project.image } alt={ project.alter }/>
+              <img src={project.image} alt={project.alter} />
             </div>
             <div className="__content_wrapper">
               <h3 className="title">
-                <TextDecrypt text={ project.id + '. ' + project.title } />
+                <TextDecrypt text={project.title} />
               </h3>
-              <p className="description">
-                { project.description }
-              </p>
+              <p className="description">{project.description}</p>
             </div>
           </div>
         ))}
